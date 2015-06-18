845809144c36d09ed8dc04a444e0dc50dc9530ae
Jaysen Parmar <jparmar@serviceradius.com>
2015-06-18 10:52:28 -0700

    Really proud

---
+++ public/js/hover.js
@@ -0,0 +1,164 @@
+
+$('#twc_30').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_30_content').html();
+        }
+});
+
+
+
+$('#twc_70').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_70_content').html();
+        }
+});
+
+$('#twc_startover').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_so_content').html();
+        }
+});
+
+$('#twc_lookback').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_lb_content').html();
+        }
+});
+
+$('#twc_hd').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_hd_content').html();
+        }
+});
+
+$('#twc_od').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_od_content').html();
+        }
+});
+
+$('#twc_vt').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_vt_content').html();
+        }
+});
+
+$('#twc_ct').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_ct_content').html();
+        }
+});
+
+
+$('#twc_dvr').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_dvr_content').html();
+        }
+});
+
+
+
+$('#twc_esp').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_esp_content').html();
+        }
+});
+
+
+$('#twc_hdc').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_hdc_content').html();
+        }
+});
+
+
+$('#twc_prem').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_prem_content').html();
+        }
+});
+
+$('#twc_mp').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_mp_content').html();
+        }
+});
+
+
+$('#twc_sports').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_sports_content').html();
+        }
+});
+
+$('#twc_hdp').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#twc_hdp_content').html();
+        }
+});
+
+$('#ch_od').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#ch_od_content').html();
+        }
+});
+
+$('#ch_ig').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#ch_ig_content').html();
+        }
+});
+
+$('#ch_dvr').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#ch_dvr_content').html();
+        }
+});
+
+$('#ch_wifi').popover({ 
+        trigger: "hover",
+        html : true, 
+        content: function() {
+          return $('#ch_wifi_content').html();
+        }
+});
+
+
+
+
