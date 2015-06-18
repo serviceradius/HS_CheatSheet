eb553b9b5634aa2037d43c2e02366f9ebaa14f95
Jaysen Parmar <jparmar@serviceradius.com>
2015-06-18 13:28:10 -0700

    added on hover for popovers

--- public/js/hover.js
+++ public/js/hover.js
@@ -1,164 +1,456 @@
-
-$('#twc_30').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_30_content').html();
+//TWC 30+ Channels 
+$('#twc_30').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_30_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-
-
-$('#twc_70').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_70_content').html();
+//TWC 70+ Channels
+ $('#twc_70').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_70_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
+    
 
-$('#twc_startover').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_so_content').html();
+
+
+//TWC Start Over
+ $('#twc_startover').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_so_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
+    
 
-$('#twc_lookback').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_lb_content').html();
+//TWC Start Over
+ $('#twc_lookback').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_lb_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#twc_hd').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_hd_content').html();
+
+//TWC HD
+ $('#twc_hd').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_hd_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#twc_od').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_od_content').html();
+
+
+//TWC Free On Demand
+ $('#twc_od').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_od_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#twc_vt').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_vt_content').html();
+
+//TWC Variety Tier
+ $('#twc_vt').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_vt_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#twc_ct').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_ct_content').html();
+
+//TWC Choice Tier
+ $('#twc_ct').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_ct_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
 
-$('#twc_dvr').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_dvr_content').html();
+
+//TWC DVR
+ $('#twc_dvr').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_dvr_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-
-
-$('#twc_esp').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_esp_content').html();
+//TWC Espanol Tier
+ $('#twc_esp').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_esp_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
 
-$('#twc_hdc').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_hdc_content').html();
+//TWC HD Channels
+ $('#twc_hdc').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_hdc_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
 
-$('#twc_prem').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_prem_content').html();
+
+//TWC Premium Channels
+ $('#twc_prem').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_prem_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#twc_mp').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_mp_content').html();
+
+//TWC Movie Pass 
+ $('#twc_mp').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_mp_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-
-$('#twc_sports').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_sports_content').html();
+//TWC Sports Pass 
+ $('#twc_sports').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_sports_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#twc_hdp').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#twc_hdp_content').html();
+//TWC HD Pass 
+ $('#twc_hdp').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#twc_hdp_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#ch_od').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#ch_od_content').html();
+
+//Charter On Demand
+ $('#ch_od').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#ch_od_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#ch_ig').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#ch_ig_content').html();
+//Charter Interactive Guide
+ $('#ch_ig').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#ch_ig_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#ch_dvr').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#ch_dvr_content').html();
+//Charter Interactive Guide
+ $('#ch_dvr').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#ch_dvr_content').html();
         }
-});
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
-$('#ch_wifi').popover({ 
-        trigger: "hover",
-        html : true, 
-        content: function() {
-          return $('#ch_wifi_content').html();
+
+//Charter WiFi
+ $('#ch_wifi').popover({
+        html: true,
+        trigger: 'manual',
+        placement: 'auto',
+        content: function () {
+            return $('#ch_wifi_content').html();
         }
-});
-
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
 
 
 
